1c192f017be2627322cc5ccd6036f71b9789740c
rambabupalla <rambabupalla522@gmail.com>
2025-09-28 20:47:45 +0530

    Added sum-of-first-n-terms-(gfg)

---
+++ Recursion/sum-of-first-n-terms-(gfg).java
@@ -0,0 +1,9 @@
+// User function Template for Java
+
+class Solution {
+    int sumOfSeries(int n) {
+        // code here
+        if(n==1) return 1;
+        return (n*n*n) + sumOfSeries(n-1);
+    }
+}
